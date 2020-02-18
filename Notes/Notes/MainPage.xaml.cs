4ea6d0bf0ff8d5d2cd2b7e6db04f54b196d79a99
Marcelo-Alvess <marceloalvesdesousa23do1@gmail.com>
2020-02-17 22:47:10 -0300

    Aula1 PAMII

---
+++ Notes/Notes/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        string _fileName =
+        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+        public MainPage()
+        {
+            InitializeComponent();
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+
+            DisplayAlert("Mensagem pra tu!", "Texto salvo com sucesso, parça", "Demorô");
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (await DisplayAlert("Mensagem pra tu", "Deseja realmente apagar o bagulho?", "Yes", "No"))
+            {
+                if (File.Exists(_fileName))
+                {
+                    File.Delete(_fileName);
+                }
+                editor.Text = string.Empty;
+            }
+        }
+    }
+}
